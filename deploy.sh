6a1eec0db8af1fe0fb9d82fdf5ff0c35218f03cb
rh-messaging-ci <messaging-infra@redhat.com>
2025-09-23 04:05:40 +0000

    Upgrade AMQ Broker Jolokia Api Server container images to 8.0.0.CON.1.SR1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DEFAULT_IMAGE=registry.redhat.io/amq-broker-8/amq-broker-80-jolokia-api-server-rhel9@sha256:17e822d161b631e0fcbc3450b0bdcf31db9ab44183d0c6c17fcedc0343ab07fd
+DEFAULT_IMAGE=registry.redhat.io/amq-broker-8/amq-broker-80-jolokia-api-server-rhel9@sha256:182124f344285ec211ed7cd9d5804bb64b2f5ce8b78a390a92e58bd3864f882e
 API_SERVER_IMAGE=${DEFAULT_IMAGE}
 
 SCRIPT_NAME=$(basename "$0")
